80c343111d48599715f17c3b07945b4dda1dc1b0
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:55:30 -0400

    Add files via upload

---
+++ ComparerChaines/ComparerChaines/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    // 
+    class Program
+    {
+        // Comparer deux chaines de caractères
+        public static bool ComparerChaines(string p_chaine1,string p_chaine2)
+        {
+            if (p_chaine1.Length == p_chaine2.Length)
+                return true;
+            else return false;
+               
+        }
+        static void Main(string[] args)
+        {
+            if (ComparerChaines("Hasna", "Didier"))
+
+                Console.WriteLine("Les deux chaines ont la même longueur.");
+            else
+                Console.WriteLine("Les deux chaines n'ont pas la même longueur.");
+            Console.ReadKey();
+        }
+    }
+}
